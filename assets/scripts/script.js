27aa182ba8a03a63097a3dbd07d37e7de2025c3e
kesavan-t-dev <kesavan.t@mitrahsoft.in>
2025-12-22 19:20:19 +0530

    rework updated

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -4,7 +4,8 @@ function search() {
     const err = document.getElementById('alertText');
     const result = document.getElementById('result_msg');
 
-    
+    const vowels = ['a', 'i', 'e', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+
     err.classList.add('d-none');
     result.classList.add('d-none');
 
@@ -16,24 +17,16 @@ function search() {
         errmsg = 'Please enter a sentence';
     } else if (/[^a-zA-Z0-9\s]/.test(input)) {
         errmsg = 'Special characters are not allowed';
-    } else {
-
-        const ch = input;
-
-        if (/\d/.test(input)) {
+    } else if (/\d/.test(input)) {
             errmsg = 'Numbers are not allowed';
-        } else if (input.length > 1) {
+    } else if (input.length > 1) {
             errmsg = 'Please enter only one word';
-        } else {
-            const vowels = ['a','i','e','o','u','A','E','I','O','U'];
-            if (vowels.includes(ch)) {
-                resultmsg = `Vowels are: ${ch}`;
-            } else {
-                errmsg = 'No Vowels Found';
-            }
-        }
+    } else if (vowels.includes(input)) {
+            resultmsg = `Vowels are: ${input}`;
+    } else {
+        errmsg = 'No Vowels Found';
     }
-
+       
     
     err.innerText = errmsg;
     result.innerHTML = resultmsg;
